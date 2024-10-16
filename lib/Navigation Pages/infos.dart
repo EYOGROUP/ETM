875ac5919b8caea6170e5a423087614151960d8f
Younes Dahbi <younesoffi.dahbi@gmail.com>
2024-10-17 00:56:37 +0200

    save work

--- lib/Navigation Pages/infos.dart
+++ lib/Navigation Pages/infos.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:day_night_switcher/day_night_switcher.dart';
 import 'package:flutter/material.dart';
+
+import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 import 'package:time_management/provider/tm_provider.dart';
 
@@ -19,29 +21,59 @@ class _InfosPageState extends State<InfosPage> {
         Provider.of<TimeManagementPovider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        leading: InkWell(
-          onTap: () => Navigator.of(context).pop(),
-          child: Icon(Platform.isIOS
-              ? Icons.arrow_back_ios_new_outlined
-              : Icons.arrow_back_outlined),
-        ),
+        automaticallyImplyLeading: false,
         title: const Text('Infos'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          ListTile(
-            title: const Text('Theme mode'),
-            trailing: DayNightSwitcher(
-              isDarkModeEnabled: tmProvider.isDarkGet,
-              onStateChanged: (bool isDarkModeEnabled) async {
-                await tmProvider.switchThemeApp(
-                    context: context, valueTheme: isDarkModeEnabled);
-                if (!mounted) return;
-              },
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: MediaQuery.of(context).size.height * 0.02,
+            horizontal: MediaQuery.of(context).size.width * 0.02),
+        child: Column(
+          children: [
+            ListTile(
+              title: const Text('Contact Us'),
+              trailing: Icon(Platform.isIOS
+                  ? Icons.arrow_forward_ios_rounded
+                  : Icons.arrow_forward_outlined),
             ),
-          )
-        ],
+            ListTile(
+              title: const Text('Give Feedback'),
+              trailing: Icon(Platform.isIOS
+                  ? Icons.arrow_forward_ios_rounded
+                  : Icons.arrow_forward_outlined),
+            ),
+            ListTile(
+              title: const Text('Privacy Policy'),
+              trailing: Icon(Platform.isIOS
+                  ? Icons.arrow_forward_ios_rounded
+                  : Icons.arrow_forward_outlined),
+            ),
+            ListTile(
+              title: const Text('Tems of Use'),
+              trailing: Icon(Platform.isIOS
+                  ? Icons.arrow_forward_ios_rounded
+                  : Icons.arrow_forward_outlined),
+            ),
+            Gap(MediaQuery.of(context).size.height * 0.02),
+            ListTile(
+              title: const Text('Theme Mode'),
+              trailing: DayNightSwitcher(
+                isDarkModeEnabled: tmProvider.isDarkGet,
+                onStateChanged: (bool isDarkModeEnabled) async {
+                  await tmProvider.switchThemeApp(
+                      context: context, valueTheme: isDarkModeEnabled);
+                  if (!mounted) return;
+                },
+              ),
+            ),
+            const Spacer(),
+            const Text.rich(TextSpan(children: [
+              TextSpan(text: 'App Version '),
+              TextSpan(text: '1.00'),
+            ]))
+          ],
+        ),
       ),
     );
   }
