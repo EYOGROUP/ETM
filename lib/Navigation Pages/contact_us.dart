25ea7f3884b516c9afc49c1bdf019216074c6a92
Younes Dahbi <younesoffi.dahbi@gmail.com>
2024-11-07 08:31:41 +0100

    save work

--- lib/Navigation Pages/contact_us.dart
+++ lib/Navigation Pages/contact_us.dart
@@ -4,8 +4,8 @@ import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 import 'package:time_management/constants.dart';
 import 'package:time_management/provider/tm_provider.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactUs extends StatefulWidget {
   const ContactUs({
@@ -29,6 +29,7 @@ class _ContactUsState extends State<ContactUs> {
     super.dispose();
     _nameController.dispose();
     _emailController.dispose();
+    _descriptionController.dispose();
   }
 
   String? encodeQueryParameters(Map<String, String> params) {
@@ -44,15 +45,15 @@ class _ContactUsState extends State<ContactUs> {
 
     WidgetsBinding.instance.addPostFrameCallback(
       (_) {
-        final getLablels = AppLocalizations.of(context)!;
+        final getLabels = AppLocalizations.of(context)!;
         _reasons = [
-          getLablels.technicalSupport,
-          getLablels.featureRequest,
-          getLablels.timeTrackingErrors,
-          getLablels.appPerformance,
-          getLablels.feedback,
-          getLablels.reportABug,
-          getLablels.other
+          getLabels.technicalSupport,
+          getLabels.featureRequest,
+          getLabels.timeTrackingErrors,
+          getLabels.appPerformance,
+          getLabels.feedback,
+          getLabels.reportABug,
+          getLabels.other
         ];
         setState(() {});
 
@@ -60,6 +61,39 @@ class _ContactUsState extends State<ContactUs> {
         tm.setOrientation(context);
       },
     );
+  }
+
+  Future<void> _sendEmail() async {
+    if (_globalKey.currentState!.validate()) {
+      try {
+        if (_reasonChoosed == '') {
+          return Constants.showInSnackBar(
+              value: AppLocalizations.of(context)!.pleaseChooseAReason,
+              context: context);
+        }
+
+        final Uri emailLaunchUri = Uri(
+          scheme: 'mailto',
+          path: 'younesoffi.dahbi@gmail.com', // Your email here
+          query: encodeQueryParameters(<String, String>{
+            'subject':
+                '$_reasonChoosed / ${AppLocalizations.of(context)!.from} (${_nameController.text})',
+            'body': _descriptionController.text,
+          }),
+        );
+
+        if (!await launchUrl(emailLaunchUri,
+            mode: LaunchMode.externalApplication)) {
+          if (!mounted) return;
+          throw Exception(
+              '${AppLocalizations.of(context)!.couldNotLaunch} $emailLaunchUri');
+        }
+      } catch (error) {
+        if (mounted) {
+          Constants.showInSnackBar(value: error.toString(), context: context);
+        }
+      }
+    }
   }
 
   @override
@@ -80,49 +114,21 @@ class _ContactUsState extends State<ContactUs> {
           ),
           actions: [
             TextButton(
-                onPressed: () async {
-                  if (_globalKey.currentState!.validate()) {
-                    try {
-                      if (_reasonChoosed == '') {
-                        return Constants.showInSnackBar(
-                            value: getLabels.pleaseChooseAReason,
-                            context: context);
-                      }
-
-                      final Uri emailLaunchUri = Uri(
-                        scheme: 'mailto',
-                        query: encodeQueryParameters(<String, String>{
-                          'subject':
-                              '$_reasonChoosed / ${getLabels.from} (${_nameController.text})',
-                          'body': _descriptionController.text,
-                          'to': 'younesoffi.dahbi@gmail.com',
-                        }),
-                      );
-
-                      if (!await launchUrl(emailLaunchUri,
-                          mode: LaunchMode.externalApplication)) {
-                        throw Exception(
-                            '${getLabels.couldNotLaunch} $emailLaunchUri');
-                      }
-                    } catch (error) {
-                      if (!context.mounted) return;
-                      Constants.showInSnackBar(
-                          value: error.toString(), context: context);
-                    }
-                  }
-                },
-                child: Text(
-                  getLabels.send,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ))
+              onPressed: _sendEmail,
+              child: Text(
+                getLabels.send,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
         body: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.05,
-                vertical: MediaQuery.of(context).size.height * 0.03),
+              horizontal: MediaQuery.of(context).size.width * 0.05,
+              vertical: MediaQuery.of(context).size.height * 0.03,
+            ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -132,42 +138,57 @@ class _ContactUsState extends State<ContactUs> {
                   fieldCaption: getLabels.enterYourFullName,
                   isFlexibelField: false,
                 ),
+                // Adding the reason selection part as before
                 Container(
-                    margin: EdgeInsets.only(
-                        bottom: MediaQuery.of(context).size.height * 0.04),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '${getLabels.reason}:*',
-                          style: const TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
+                  margin: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).size.height * 0.04),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${getLabels.reason}:*',
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Gap(MediaQuery.of(context).size.height * 0.02),
-                        DropdownButton(
-                          iconEnabledColor:
-                              Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(
-                              MediaQuery.of(context).size.height * 0.02),
-                          value: _reasonChoosed != '' ? _reasonChoosed : null,
-                          items: _reasons
-                              ?.map((reason) => DropdownMenuItem(
-                                  value: reason, child: Text(reason)))
-                              .toList(),
-                          hint: Text(getLabels.chooseAReason),
-                          onChanged: (value) {
-                            setState(() {
-                              _reasonChoosed = value!;
-                            });
-                          },
-                        ),
-                      ],
-                    )),
+                      ),
+                      Gap(MediaQuery.of(context).size.height * 0.02),
+                      DropdownButton(
+                        iconEnabledColor: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(
+                            MediaQuery.of(context).size.height * 0.02),
+                        value: _reasonChoosed != '' ? _reasonChoosed : null,
+                        items: _reasons
+                            ?.map((reason) => DropdownMenuItem(
+                                value: reason, child: Text(reason)))
+                            .toList(),
+                        hint: Text(getLabels.chooseAReason),
+                        onChanged: (value) {
+                          setState(() {
+                            _reasonChoosed = value!;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
                 TitleWithTextfield(
                   controller: _descriptionController,
                   fieldName: '${getLabels.description}:*',
                   fieldCaption: '${getLabels.write}...',
                   isFlexibelField: true,
+                ),
+                // Providing clear contact information (email or phone)
+                Gap(MediaQuery.of(context).size.height * 0.01),
+                Text(
+                  '${getLabels.contactDetails}:',
+                  style: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                Gap(MediaQuery.of(context).size.height * 0.01),
+                Text(
+                  '${getLabels.email}: younesoffi.dahbi@gmail.com', // Replace with your support email
+                  style: const TextStyle(fontSize: 18),
                 ),
               ],
             ),
@@ -207,9 +228,8 @@ class TitleWithTextfield extends StatelessWidget {
           ),
           Gap(MediaQuery.of(context).size.height * 0.02),
           TextFormField(
-            maxLines: isFlexibelField ? 5 : null,
+            maxLines: isFlexibelField ? 5 : 1,
             maxLength: isFlexibelField ? 300 : null,
-            keyboardType: TextInputType.name,
             decoration: InputDecoration(
                 hintText: fieldCaption,
                 hintStyle: const TextStyle(fontSize: 14.0)),
