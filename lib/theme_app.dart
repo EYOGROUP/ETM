9d38cbc79673491fe7c527050ab7730ca7a180b3
Younes Dahbi <younesoffi.dahbi@gmail.com>
2024-11-12 19:46:09 +0100

    save

--- lib/theme_app.dart
+++ lib/theme_app.dart
@@ -1,7 +1,3 @@
-// Theme config for FlexColorScheme version 7.3.x. Make sure you use
-// same or higher package version, but still same major version. If you
-// use a lower package version, some properties may not be supported.
-// In that case remove them after copying this theme to your app.
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 class ThemeApp {
@@ -37,10 +33,5 @@ class ThemeApp {
     visualDensity: FlexColorScheme.comfortablePlatformDensity,
     useMaterial3: true,
     swapLegacyOnMaterial3: true,
-    // To use the Playground font, add GoogleFonts package and uncomment
-    // fontFamily: GoogleFonts.notoSans().fontFamily,
   );
-// If you do not have a themeMode switch, uncomment this line
-// to let the device system mode control the theme mode:
-// themeMode: ThemeMode.system,
 }
